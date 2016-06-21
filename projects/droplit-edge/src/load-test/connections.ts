d9d8028da97efb403a28edd212080e77dc6045c7
Chris Woodle <Chris@droplit.io>
2016-06-21 00:37:15 -0400

    Fixed getMac spawning

--- projects/droplit-edge/src/load-test/connections.ts
+++ projects/droplit-edge/src/load-test/connections.ts
@@ -1,40 +1,48 @@
 import Transport from '../transport';
 import * as debug from 'debug';
 
+let localSettings = require('../../localsettings.json');
+
 let log = debug('droplit:load-test-connections');
 
 let config: any = require('../../test-config.json');
 
 
-
 function start() {
-    // TODO: Count maximum supported connections
-    let totalConnections = config.loadTest.numConnections;
-  let count = 0;
- for (let ii = 0; ii < config.loadTest.numConnections; ii++) {
-    startConnection((connected) => {
-         console.log(connected);
-         if ( connected) {
-        count++;
-         console.log(count);
-     }
+    getEdgeId((edgeId) => {
+        let connections = 0;
+        // TODO: Count maximum supported connections
+        let totalConnections = config.loadTest.numConnections;
+        let count = 0;
+        for (let ii = 0; ii < config.loadTest.numConnections; ii++) {
+            console.log("Sent", ii);
+            startConnection(edgeId, (connected) => {
+                count++;
+                console.log(connected);
+                if(connected){
+                    connections++;
+                }
+                console.log(ii);
+                if(count === config.loadTest.numConnections - 1) {
+                    console.log("count", count);
+                    console.log("connections", connections);
+                    
+                }
+            });
+        }
     });
- }
-   console.log(count);
+
 }
 
 
-function startConnection(callback: (connected: boolean) => void ) {
+function startConnection(edgeId: string, callback: (connected: boolean) => void) {
     let transport = new Transport();
-    getEdgeId((edgeId) => {
-        let localSettings = require('../../localsettings.json');
-        transport.start(config.transport, {
-            "x-edge-id": edgeId,
-            "x-ecosystem-id": localSettings.ecosystemId
-        }, callback);
-    
-        });
-    };
+    transport.start(config.transport, {
+        "x-edge-id": edgeId,
+        "x-ecosystem-id": localSettings.ecosystemId
+    }, callback);
+
+};
 let _edgeId: string = undefined;
 
 function getEdgeId(callback: (edgeId: string) => void) {
