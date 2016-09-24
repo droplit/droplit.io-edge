65a42d16080dbfa93c91494b28059d3bbf9039da
ferrantejake <jake.ferrante@hotmail.com>
2016-09-24 12:35:18 -0400

    Add DroplitConnectivity as dependency touchup

--- archive/plugin/pluginLoader.ts
+++ archive/plugin/pluginLoader.ts
@@ -4,14 +4,11 @@ import * as fs from 'fs';
 process.on('message', function(data: any) {
     const options = {
         console: 'inherit',
-        sandbox: {
-            
-        },
+        sandbox: { },
         require: true,
         requireExternal: true
     };
     const vm = new NodeVM(options);
     const code = fs.readFileSync(require.resolve(data.path));
     const mod = vm.run(code);
-    
 });
