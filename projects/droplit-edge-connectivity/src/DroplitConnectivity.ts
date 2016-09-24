89baf7fb67e7d73ed11eb29c6f412f9627fb8e1b
ferrantejake <jake.ferrante@hotmail.com>
2016-09-24 18:34:15 -0400

    Remove for localPlugin

--- projects/droplit-edge-connectivity/src/DroplitConnectivity.ts
+++ projects/droplit-edge-connectivity/src/DroplitConnectivity.ts
@@ -2,7 +2,7 @@ import * as droplit from 'droplit-plugin';
 
 console.log('droplit connectivity');
 
-export class DroplitConnectivity extends droplit.DroplitPlugin {
+export class DroplitConnectivity extends droplit.DroplitLocalPlugin {
 
     services: any;
 
@@ -16,42 +16,6 @@ export class DroplitConnectivity extends droplit.DroplitPlugin {
         };
     }
 
-    // virtual device tracking
-    private deviceConnected: { [localId: string]: boolean } = {};
-
-    /**
-     * Example plugin will produce two devices when told to discover
-     */
-
-    public discover() {
-        setImmediate(() => { // simulate async
-            this.onDeviceInfo({
-                localId: '.',
-                services: ['Connectivity'],
-                promotedMembers: {
-                    'status': 'Connectivity.status'
-                }
-            });
-            this.onDiscoverComplete();
-        });
-    }
-
-    public connect(localId: string): boolean {
-        // track state changes on this device
-        this.deviceConnected[localId] = true;
-        return true;
-    }
-
-    public disconnect(localId: string): boolean {
-        // stop tracking state changes on this device
-        this.deviceConnected[localId] = false;
-        return true;
-    }
-
-    public dropDevice(localId: string): boolean {
-        this.disconnect(localId);
-        return true;
-    }
 
     public getStatus(localId: string, callback: (result: any) => void) {
         callback('online');
