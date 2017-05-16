ccdc8270087537804164a74c70936fe97c660c93
NikDoukellis <nik@droplit.io>
2017-05-16 19:43:20 -0400

    Fix make node-getmac ES5 compatible

--- projects/droplit-edge/src/node-getmac.js
+++ projects/droplit-edge/src/node-getmac.js
@@ -1,16 +1,16 @@
 // Forked from https://www.npmjs.com/package/node-getmac
-const localSettings = require('../localsettings.json');
+var localSettings = require('../localsettings.json');
 var execSync = require('child_process').execSync;
 var platform = process.platform;
 
 module.exports = (function () {
-    const linuxNetInterface = (localSettings.config && localSettings.config.LinuxNetInterfaceOverride) ?
+    var linuxNetInterface = (localSettings.config && localSettings.config.LinuxNetInterfaceOverride) ?
         localSettings.config.LinuxNetInterfaceOverride :
         'eth0';
     var cmd = {
         win32: 'getmac',
         darwin: 'ifconfig -a',
-        linux: `cat /sys/class/net/${linuxNetInterface}/address`
+        linux: 'cat /sys/class/net/' + linuxNetInterface + '/address'
     }[platform];
 
     var regStr = '((?:[a-z0-9]{2}[:-]){5}[a-z0-9]{2})';
