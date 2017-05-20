94c174d7bbf2194d27d17cb4475c781c93c01b42
NikDoukellis <nik@droplit.io>
2017-05-20 00:45:40 -0400

    Improve diagnostic command system
    - Refactor to allow commands with arguments instead of the `pmsg` command being a special case.
    
    - Add descriptions to the commands that are viewable through the `help` command.
    
    - Fix indent on `ping` command error output.

--- projects/droplit-edge/src/diagnostics.ts
+++ projects/droplit-edge/src/diagnostics.ts
@@ -40,86 +40,119 @@ module.exports = (router: any) => {
         socket.write('Edge Diagnostics console\n\r');
 
         const commands: any = {
-            discovered: () => {
-                router.plugins.forEach((plugin: any) => {
-                    const discovered = cache.getDevicesByPlugin(plugin.pluginName);
-                    if (discovered.length > 0) {
-                        socket.write(`  ${plugin.pluginName}\n\r`);
-                        discovered.forEach(device =>
-                            socket.write(`    ${device.deviceId}\n\r`));
-                    }
-                });
+            crash: {
+                desc: 'Throw an error.',
+                exec: () => {
+                    throw new Error('This crash is intentional');
+                }
             },
-            exit: () =>
-                socket.end('Goodbye!\n\r'),
-            help: () =>
-                socket.write(`  ${Object.keys(commands).join(', ')}\n\r`),
-            local: () =>
-                socket.write(`  ${JSON.stringify(settings)}\n\r`),
-            pmsg: (plugin: string, message: string) => {
-                router.sendPluginMessage({ plugin, message }, (value: any) => {
-                    if (Array.isArray(value)) {
-                        let output = '';
-                        value.forEach(v => {
-                            if (v.key && typeof v.key === 'string' && v.value && Array.isArray(v.value)) {
-                                output += `  ${v.key}\n\r`;
-                                if (v.value.length > 0)
-                                    output += `    ${v.value.join('\n\r    ')}\n\r`;
-                                else
-                                    output += '    (none)\n\r';
-                            } else
-                                output = `  ${value.join('\n\r  ')}\n\r`;
-                        });
-                        socket.write(output);
-                    }
-                    else if (typeof value === 'string')
-                        socket.write(value);
-                    else
-                        socket.write(value.toString());
-                });
+            discovered: {
+                desc: 'List the cached deviceIds by plugin.',
+                exec: () =>
+                    router.plugins.forEach((plugin: any) => {
+                        const discovered = cache.getDevicesByPlugin(plugin.pluginName);
+                        if (discovered.length > 0) {
+                            socket.write(`  ${plugin.pluginName}\n\r`);
+                            discovered.forEach(device =>
+                                socket.write(`    ${device.deviceId}\n\r`));
+                        }
+                    })
             },
-            ping: () => {
-                socket.write('  send message...\n\r');
-                const data = {
-                    edgeId: router.macAddress,
-                    time: new Date()
-                };
-                router.transport.sendRequest('diagnostics', data, (res: any, err: any) => {
-                    if (err)
-                        return socket.write(  `error: ${JSON.stringify(err)}\n\r`);
-                    socket.write(`  response: ${JSON.stringify(res)}\n\r`);
-                });
+            exit: {
+                desc: 'Exit the diagnostic terminal.',
+                exec: () =>
+                    socket.end('Goodbye!\n\r'),
             },
-            plugins: () =>
-                socket.write(`  ${(Array as any).from(router.plugins.keys()).join(',\n\r  ')}\n\r`),
-            reset: () => {
-                throw new Error('This crash is intentional');
+            help: {
+                desc: 'Show commands.',
+                exec: () =>
+                    Object.keys(commands).forEach(key => {
+                        const command = commands[key];
+                        socket.write(`  ${key}\n\r`);
+                        if (command.desc)
+                            socket.write(`    ${command.desc}\n\r`);
+                    })
             },
-            socket: () => {
-                const readyState = router.transport.getReadyState();
-                const state = (readyState === undefined) ? 'undefined' :
-                            (readyState === WebSocket.CLOSED) ? 'closed' :
-                            (readyState === WebSocket.CLOSING) ? 'closing' :
-                            (readyState === WebSocket.CONNECTING) ? 'connecting' :
-                            (readyState === WebSocket.OPEN) ? 'open' :
-                            'unknown';
-                socket.write(`  current time:           ${new Date().toISOString()}\n\r`);
-                socket.write(`  last connected at:      ${dxData.connected ? dxData.connected.toISOString() : null}\n\r`);
-                socket.write(`  last heartbeat attempt: ${dxData.lastHeartbeatAttempt ? dxData.lastHeartbeatAttempt.toISOString() : null}\n\r`);
-                socket.write(`  last heartbeat:         ${dxData.lastHeartbeat ? dxData.lastHeartbeat.toISOString() : null}\n\r`);
-                socket.write(`  last message:           ${dxData.lastMessage ? dxData.lastMessage.toISOString() : null}\n\r`);
-                socket.write(`  state:                  ${state}\n\r`);
+            local: {
+                desc: 'Show local settings.',
+                exec: () =>
+                    socket.write(`  ${JSON.stringify(settings)}\n\r`)
+            },
+            pmsg: {
+                desc: 'Send a message to a specified plugin. Command format: `pmsg <plugin> <message>`.',
+                exec: (plugin: string, message: string) =>
+                    router.sendPluginMessage({ plugin, message }, (value: any) => {
+                        if (Array.isArray(value)) {
+                            let output = '';
+                            value.forEach(v => {
+                                if (v.key && typeof v.key === 'string' && v.value && Array.isArray(v.value)) {
+                                    output += `  ${v.key}\n\r`;
+                                    if (v.value.length > 0)
+                                        output += `    ${v.value.join('\n\r    ')}\n\r`;
+                                    else
+                                        output += '    (none)\n\r';
+                                } else
+                                    output = `  ${value.join('\n\r  ')}\n\r`;
+                            });
+                            socket.write(output);
+                        }
+                        else if (typeof value === 'string')
+                            socket.write(value);
+                        else
+                            socket.write(value.toString());
+                    })
+            },
+            ping: {
+                desc: 'Send a test message to the WS server.',
+                exec: () => {
+                    socket.write('  send message...\n\r');
+                    const data = {
+                        edgeId: router.macAddress,
+                        time: new Date()
+                    };
+                    router.transport.sendRequest('diagnostics', data, (res: any, err: any) => {
+                        if (err)
+                            return socket.write(`  error: ${JSON.stringify(err)}\n\r`);
+                        socket.write(`  response: ${JSON.stringify(res)}\n\r`);
+                    });
+                }
+            },
+            plugins: {
+                desc: 'List loaded plugins.',
+                exec: () =>
+                    socket.write(`  ${(Array as any).from(router.plugins.keys()).join('\n\r  ')}\n\r`)
+            },
+            socket: {
+                desc: 'Display the state of the transport web socket.',
+                exec: () => {
+                    const readyState = router.transport.getReadyState();
+                    const state =
+                        (readyState === undefined) ? 'undefined' :
+                        (readyState === WebSocket.CLOSED) ? 'closed' :
+                        (readyState === WebSocket.CLOSING) ? 'closing' :
+                        (readyState === WebSocket.CONNECTING) ? 'connecting' :
+                        (readyState === WebSocket.OPEN) ? 'open' :
+                        'unknown';
+                    socket.write(`  current time:           ${new Date().toISOString()}\n\r`);
+                    socket.write(`  last connected at:      ${dxData.connected ? dxData.connected.toISOString() : null}\n\r`);
+                    socket.write(`  last heartbeat attempt: ${dxData.lastHeartbeatAttempt ? dxData.lastHeartbeatAttempt.toISOString() : null}\n\r`);
+                    socket.write(`  last heartbeat:         ${dxData.lastHeartbeat ? dxData.lastHeartbeat.toISOString() : null}\n\r`);
+                    socket.write(`  last message:           ${dxData.lastMessage ? dxData.lastMessage.toISOString() : null}\n\r`);
+                    socket.write(`  state:                  ${state}\n\r`);
+                }
             }
         };
 
         rl.on('line', line => {
-            const pluginMessage = /^pmsg\s([\w-]+)(?:\s([\w-]+))?$/.exec(line);
-            if (pluginMessage)
-                commands.pmsg(pluginMessage[1], pluginMessage[2]);
-            else if (commands[line])
-                commands[line]();
-            else
-                socket.write(`Unknown command: ${line}\n\r`);
+            const parsed = /^(\S+)(?:\s(.+))?$/.exec(line);
+            if (parsed && commands[parsed[1]]) {
+                const command = commands[parsed[1]];
+                const params = parsed[2] ? parsed[2].split(' ') : [];
+                if (command.exec)
+                    return command.exec(...params);
+            }
+
+            socket.write(`Unknown command: ${line}\n\r`);
         });
 
         socket.on('end', () => {
