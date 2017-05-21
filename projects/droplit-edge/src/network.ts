b1f0c2b8eb4d4bb3051a4303f343b83645bc7d41
Sabbir A. Baser <sabbir@droplit.io>
2017-05-21 13:56:34 -0400

    No need to send status: 200 in the response object

--- projects/droplit-edge/src/network.ts
+++ projects/droplit-edge/src/network.ts
@@ -26,7 +26,6 @@ export class Network {
                 res.setHeader('Content-Type', 'application/json');
                 res.statusCode = 200;
                 const result = {
-                    status: 200,
                     edgeId: this.edgeId
                 };
                 console.log('REQ: RECEIVED: ');
@@ -43,7 +42,6 @@ export class Network {
                     console.log(stdout);
                     wifis = stdout;
                     const result = {
-                        status: 200,
                         items: wifis
                     };
                     res.end(JSON.stringify(result));
@@ -60,7 +58,6 @@ export class Network {
                         console.log(error);
                     } else {
                         result = {
-                            status: 200,
                             message: `Connected to AP: ${(<any>req).body.SSID}`
                         };
                     }
