fdc4f542a919f31ef1984d4673f8950b3135274e
Jarod <jrd165smith@gmail.com>
2017-05-22 18:29:43 -0400

    refactored to handle edge cases with bad requests and handle security modes server side

--- projects/droplit-edge/src/network.ts
+++ projects/droplit-edge/src/network.ts
@@ -4,32 +4,28 @@ import * as debug from 'debug';
 const router = require('router')();
 const bodyParser = require('body-parser');
 const log = debug('droplit:network');
-const localSettings = require('../localsettings.json');    
+const localSettings = require('../localsettings.json');
 let PORT: number;
 let server: http.Server;
 let SSID: string;
 
-
-module.exports = ( edgeId: string) => {
+module.exports = (edgeId: string) => {
     SSID = edgeId.replace(new RegExp('[:-]+', 'g'), '');
     PORT = 81;
     if (localSettings.config && localSettings.config.portOverride) {
-        log('Starting Edge server on ' + localSettings.config.portOverride);
+        log(`Starting Edge server on ${localSettings.config.portOverride}`);
         PORT = localSettings.config.portOverride;
     }
-    
-    edgeId = edgeId;
+
     log(edgeId);
-    SSID = SSID
     log(SSID);
-    SSID = 'hub_' + SSID.slice(SSID.length - 4, SSID.length);
+    SSID = `hub_${SSID.slice(SSID.length - 4, SSID.length)}`;
     server = http.createServer((request, response) => {
         router(request, response, require('finalhandler')(request, response));
     });
 
     log(SSID);
     createWap(SSID);
-
 
     server.listen(PORT, () => {
         log('Edge Server Listening on Port:', PORT);
@@ -42,7 +38,7 @@ module.exports = ( edgeId: string) => {
             res.setHeader('Content-Type', 'application/json');
             res.statusCode = 200;
             const result = {
-                edgeId: edgeId
+                edgeId
             };
             log('REQ: RECEIVED: ');
             res.end(JSON.stringify(result));
@@ -52,80 +48,132 @@ module.exports = ( edgeId: string) => {
             res.setHeader('Content-Type', 'application/json');
             res.statusCode = 200;
             let wifis: any[] = [];
-            let childProcess = require('child_process');
-            // wifis = parseWifi("[Stanley Homes Inc\n][TKIP][PSK] [Stanley Homes Inc-guest][OPEN][] [Foxconn OEM][OPEN][] [droplit][CCMP][PSK] [CableWiFi] [OPEN][]");
+            const childProcess = require('child_process');
+            // wifis = parseWifi('[Stanley Homes Inc\n][TKIP][PSK] [Stanley Homes Inc-guest][OPEN][] [Foxconn OEM][OPEN][] [droplit][CCMP][PSK] [CableWiFi] [OPEN][]');
 
             childProcess.exec('scanWifi', (error: any, stdout: any, stderr: any) => {
-                if (error)
+                if (error) {
+                    res.statusCode = 501;
+                    const message = {message: `${error}`};
+                    res.end(JSON.stringify(message));
                     log(error);
+                }
                 log(stdout);
                 wifis = parseWifi(stdout);
 
-                let result: Object = {
+                const result: Object = {
                     items: wifis
                 };
                 res.end(JSON.stringify(result));
             });
 
         })
-        .put((req: http.ClientRequest, res: http.ServerResponse) => {
+        .put((req: any, res: http.ServerResponse) => {
             res.setHeader('Content-Type', 'application/json');
             res.statusCode = 200;
             let result: any;
-            let childProcess = require('child_process');
+            let command = '';
+            let wifis = [];
+            let theWifi: any;
+            const childProcess = require('child_process');
             log((<any>req).body);
-
-            childProcess.exec(`connectWiFi ${(<any>req).body.SSID} ${(<any>req).body.MODE} ${(<any>req).body.PASS}`, (error: any, stdout: any, stderr: any) => {
-                if (error) {
-                    log(error);
-                } else {
-                    res.statusCode = 200;
-                    result = {
-                        message: `Connected to AP: ${(<any>req).body.SSID}`
-                    };
-                }
-                log(stdout);
-            });
-            res.end(JSON.stringify(result));
+            if (req.body && req.body.SSID) {
+                childProcess.exec('scanWifi', (error: any, stdout: any, stderr: any) => {
+                    if (error)
+                        log(error);
+                    log(stdout);
+                    wifis = parseWifi(stdout);
+                    wifis.forEach(element => {
+                        if (element.SSID === req.body.SSID) {
+                            theWifi = element;
+                        }
+                    });
+                    if (theWifi.MODE) {
+                        switch (theWifi.MODE) {
+                            case 'PSK':
+                                command = `connectWiFi ${req.body.SSID} psk-mixed ${req.body.PASS}`;
+                                break;
+                            case 'WPA':
+                                command = `connectWiFi ${req.body.SSID} wpa-mixed ${req.body.PASS}`;
+                            default:
+                                command = `connectWiFi ${req.body.SSID} ${theWifi.MODE} ${req.body.PASS}`;
+                                break;
+                        }
+                        log(`command: ${command}`);
+                        childProcess.exec(command, (error: any, stdout: any, stderr: any) => {
+                            if (error) {
+                                log(error);
+                            } else {
+                                res.statusCode = 200;
+                                result = {
+                                    message: `Connected to AP: ${(<any>req).body.SSID}`
+                                };
+                            }
+                            log(stdout);
+                        });
+                        res.end(JSON.stringify(result));
+                    }
+                    else {
+                        command = `connectWiFi ${req.body.SSID}`;
+                        log(`command: ${command}`);
+                        childProcess.exec(command, (error: any, stdout: any, stderr: any) => {
+                            if (error) {
+                                log(error);
+                            } else {
+                                res.statusCode = 200;
+                                result = {
+                                    message: `Connected to AP: ${(<any>req).body.SSID}`
+                                };
+                            }
+                            log(stdout);
+                        });
+                        res.end(JSON.stringify(result));
+                    }
+                });
+            }
+            else {
+                res.statusCode = 400;
+                res.end(JSON.stringify({ message: 'malformed request' }));
+            }
         });
     function parseWifi(wifi_string: string): any[] {
-        log('unparsed_string: ' + wifi_string);
+        log(`unparsed_string: ${wifi_string}`);
 
-        let wifis: any[] = [];
+        const wifis: any[] = [];
         wifi_string = wifi_string.replace('\n', '');
-        log('string: ' + wifi_string);
-        let parsedWifi: string[] = wifi_string.split('');
-        let counter: number = 0;
-        let counter2: number = -1;
-        let counter3: number = 0;
-        let counter4: number = 0;
+        log(`string: ${wifi_string}`);
+        const parsedWifi: string[] = wifi_string.split('');
+        let counter = 0;
+        let counter2 = -1;
+        let counter3 = 0;
+        let counter4 = 0;
         let doConcat = true;
-        let name: string = "";
-        let mode: string = "";
-        //log('parsedWifi: '+parsedWifi);
-        for (var c in parsedWifi) {
+        let name = '';
+        let mode = '';
+        // log('parsedWifi: '+parsedWifi);
+        for (const c in parsedWifi) {
 
-            //log('isVariable: ' + isVariable);
-            //log('Character: ' + parsedWifi[c]);
+            // log('isVariable: ' + isVariable);
+            // log('Character: ' + parsedWifi[c]);
             doConcat = true;
-            if (parsedWifi[c] == '[') {
+            if (parsedWifi[c] === '[') {
                 doConcat = false;
                 counter2++;
                 counter4++;
             }
-            if (parsedWifi[c] == ']') {
+            if (parsedWifi[c] === ']') {
                 doConcat = false;
-                if (counter4 % 3 == 0) {
+                if (counter4 % 3 === 0) {
                     // log('name: ' + name);
                     // log('mode: ' + mode);
                     name = name.trim();
                     mode = mode.trim();
                     wifis[counter3] = {
-                        "SSID": name,
-                        "MODE": mode
+                        SSID: name,
+                        MODE: mode
                     };
-                    name = "";
-                    mode = "";
+                    name = '';
+                    mode = '';
                     counter3++;
                 }
 
@@ -133,10 +181,10 @@ module.exports = ( edgeId: string) => {
             }
             // log('mode: ' + mode + ' ' + counter4);
             // log('name: ' + name + ' ' + counter2);
-            if (doConcat && (counter2 % 3 == 0 || counter2 == 0)) {
+            if (doConcat && (counter2 % 3 === 0 || counter2 === 0)) {
                 name += parsedWifi[c];
             }
-            if (doConcat && counter4 % 3 == 0) {
+            if (doConcat && counter4 % 3 === 0) {
                 mode += parsedWifi[c];
             }
         }
@@ -144,12 +192,12 @@ module.exports = ( edgeId: string) => {
         return wifis;
     }
     function createWap(SSID: string) {
-        let childProcess = require('child_process'), createWAP;
-        let command = 'createWAP ' + SSID;
+        const childProcess = require('child_process');
+        const command = `createWAP ${SSID}`;
         log(command);
-        createWAP = childProcess.exec(command, (error: any, stdout: any, stderr: any) => {
+        childProcess.exec(command, (error: any, stdout: any, stderr: any) => {
             log(stdout);
         });
     }
 
-}
+};
