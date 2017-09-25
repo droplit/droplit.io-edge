05f5340d8e1c258af14116c636d4045ad256e267
Chris Woodle <chris@droplit.io>
2017-09-25 15:48:06 -0400

    Updated web api for new network scanning.

--- projects/droplit-edge/src/network.ts
+++ projects/droplit-edge/src/network.ts
@@ -7,15 +7,26 @@ const localSettings = require('../localsettings.json');
 let PORT: number;
 let server: http.Server;
 
-export interface WifiObject {
-    address: string;
-    essid: string;
+export interface IwInfoObject {
+    mac: string;
+    ssid: string;
     mode: string;
     channel: string;
     signal: string;
     quality: string;
     encryption: string;
     uci: string;
+}
+
+// For UCI options, see:
+// https://wiki.openwrt.org/doc/uci/wireless#common_interface_options
+// https://wiki.openwrt.org/doc/uci/wireless#wpa_modes
+
+export interface IwObject {
+    encryption: boolean;
+    mac: string;
+    ssid: string;
+    signal: string;
 }
 
 export const Network = (edgeId: string) => {
@@ -34,66 +45,106 @@ export const Network = (edgeId: string) => {
 
     router.use(bodyParser.json());
     router.route('/droplit-edge')
-        .get((req: any, res: http.ServerResponse) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode = 200;
-            const result = {
-                edgeId
-            };
-            log(`GET /droplit-edge ${res.statusCode}`);
-            res.end(JSON.stringify(result));
-        });
+        .get(getEdgeId);
     router.route('/droplit-edge/config/wifi')
-        .get((req: http.ServerRequest, res: http.ServerResponse) => {
-            res.setHeader('Content-Type', 'application/json');
-            scanWifi().then(items => {
-                res.statusCode = 200;
-                res.end(JSON.stringify({ items }));
-                log(`GET /droplit-edge/config/wifi ${res.statusCode}`);
-            }).catch(error => {
-                res.statusCode = 400;
-                res.end(error);
-                log(`GET /droplit-edge/config/wifi ${res.statusCode}`);
-            });
-        })
-        .put((req: any, res: http.ServerResponse) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode = 200;
-            log(`request body: ${JSON.stringify((<any>req).body, null, 2)}`);
-            if (req.body && req.body.SSID) {
-                scanWifi()
-                    .then(networks => {
-                        networks = networks.filter(network => network.essid === req.body.SSID);
-                        if (networks.length === 1) {
-                            return networks[0];
-                        } else {
-                            const message = `Could not connect to ${req.body.SSID}. Network ${req.body.SSID} not found!`;
-                            res.statusCode = 400;
-                            res.end(JSON.stringify({ message }));
-                            log(`PUT /droplit-edge/config/wifi ${res.statusCode}`);
-                            return Promise.reject(message);
-                        }
-                    })
-                    .then(() => {
-                        log(`PUT /droplit-edge/config/wifi ${res.statusCode}`);
-                        res.end();
-                        // https://wiki.openwrt.org/doc/uci/wireless#common_interface_options
-                        // https://wiki.openwrt.org/doc/uci/wireless#wpa_modes
-                        connectWiFi(req.body.SSID, req.body.PASS, req.body.AUTH_SUITE);
-                    })
-                    .catch(error => log(error));
-            } else {
-                res.statusCode = 400;
-                res.end(JSON.stringify({ message: 'malformed request' }));
-                log(`PUT /droplit-edge/config/wifi ${res.statusCode}`);
-            }
-        });
+        .get(getWifi)
+        .put(setWifi);
+    router.route('/droplit-edge/config/full-wifi')
+        .get(getFullWifi);
 
-    function scanWifi(): Promise<WifiObject[]> {
+    function getEdgeId(req: any, res: http.ServerResponse) {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200;
+        log(`GET /droplit-edge ${res.statusCode}`);
+        return res.end(JSON.stringify({
+            edgeId
+        }));
+    }
+
+    function getWifi(req: http.ServerRequest, res: http.ServerResponse) {
+        res.setHeader('Content-Type', 'application/json');
+        iwScan().then(items => {
+            res.statusCode = 200;
+            log(`GET /droplit-edge/config/wifi ${res.statusCode}`);
+            return res.end(JSON.stringify({ items }));
+        }).catch(error => {
+            res.statusCode = 500;
+            log(`GET /droplit-edge/config/wifi ${res.statusCode}`);
+            return res.end(error);
+        });
+    }
+
+    function setWifi(req: any, res: http.ServerResponse) {
+        res.setHeader('Content-Type', 'application/json');
+        log(`request body: ${JSON.stringify((<any>req).body, null, 2)}`);
+
+        if (!req.body || !req.body.ssid) {
+            res.statusCode = 400;
+            log(`PUT /droplit-edge/config/wifi ${res.statusCode}`);
+            return res.end(JSON.stringify({ message: 'malformed request' }));
+        }
+
+        // Open network
+        if (!req.body.key) {
+            res.statusCode = 200;
+            log(`PUT /droplit-edge/config/wifi ${res.statusCode}`);
+            res.end();
+            return clientMode(req.body.ssid);
+        }
+
+        // Network encryption type supplied, try and use it.
+        if (req.body.encryption) {
+            res.statusCode = 200;
+            log(`PUT /droplit-edge/config/wifi ${res.statusCode}`);
+            res.end();
+            return clientMode(req.body.ssid, req.body.key, req.body.encryption);
+        }
+
+        // Only ssid and key supplied
+        // Switch to wifi client, scan, and connect.
+        res.statusCode = 200;
+        log(`PUT /droplit-edge/config/wifi ${res.statusCode}`);
+        res.end();
+        log(`Entering client mode...`);
+        clientMode('temp').then(() => {
+            log(`Attempting to scan while in client mode...`);
+            let uci: any = null;
+            iwinfoScan()
+                .then(networks => {
+                    networks = networks.filter(network => network.ssid === req.body.ssid);
+                    if (networks.length >= 1) {
+                        uci = networks[0].uci;
+                    } else {
+                        return Promise.reject(`Could not find ${req.body.ssid}.`);
+                    }
+                })
+                .then(network => {
+                    if (!uci) return Promise.reject(`Could not determine encryption type for ${req.body.ssid}.`);
+                    log(`Attempting to connect to ${req.body.ssid} with an encryption type of ${uci}...`);
+                    clientMode(req.body.ssid, req.body.key, uci);
+                })
+                .catch(error => log(error));
+        });
+    }
+
+    function getFullWifi(req: http.ServerRequest, res: http.ServerResponse) {
+        res.setHeader('Content-Type', 'application/json');
+        iwinfoScan().then(items => {
+            res.statusCode = 200;
+            log(`GET /droplit-edge/config/wifi ${res.statusCode}`);
+            return res.end(JSON.stringify({ items }));
+        }).catch(error => {
+            res.statusCode = 500;
+            log(`GET /droplit-edge/config/wifi ${res.statusCode}`);
+            return res.end(error);
+        });
+    }
+
+    function iwinfoScan(): Promise<IwInfoObject[]> {
         return new Promise((resolve, reject) => {
             try {
                 const network = require('/usr/bin/scanWifi.js');
-                network.scanWifi((items: WifiObject[]) => {
+                network.fullScanWifi((items: IwInfoObject[]) => {
                     log(items);
                     resolve(items);
                 });
@@ -105,8 +156,24 @@ export const Network = (edgeId: string) => {
         });
     }
 
-    // OpenWRT Scan and Connect WiFi interfaces
-    function connectWiFi(SSID: string, password?: string, authSuite?: string) {
+    function iwScan(): Promise<IwObject[]> {
+        return new Promise((resolve, reject) => {
+            try {
+                const network = require('/usr/bin/scanWifi.js');
+                network.simpleScanWifi((items: IwObject[]) => {
+                    log(items);
+                    resolve(items);
+                });
+            }
+            catch (error) {
+                console.log(error);
+                reject(error);
+            }
+        });
+    }
+
+    // Connect WiFi interfaces
+    function clientMode(SSID: string, password?: string, authSuite?: string) {
         return new Promise(res => {
             const childProcess = require('child_process');
             log(`Connecting to ${SSID}${authSuite ? ` with Auth Suite ${authSuite}` : ''}`);
@@ -128,7 +195,6 @@ export const Network = (edgeId: string) => {
     }
 
     return {
-        connectWiFi,
-        scanWifi
+        clientMode
     };
 };
